8bd4701f2666ca468b210812ebd8a86656c287ce
Evri20 <158607847+Evri20@users.noreply.github.com>
2025-10-06 11:51:56 -0400

    puzzle made

--- sliderpuzzle/Assets/scripts/GameManager.cs
+++ sliderpuzzle/Assets/scripts/GameManager.cs
@@ -16,43 +16,32 @@ public class GameManager : MonoBehaviour {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 Transform piece = Instantiate(piecePrefab, gameTransform);
+                // pieces will be in a game board going from -1 to +1.
+                piece.localPosition = new Vector3(-1 + (2 * width * col) + width, +1 - (2 * width * row) - width, 0);
+
+                piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+                piece.name = $"{(row * size) + col}";
+                // we want an empty space in the bottom right.
+                if ((row == size - 1) && (col == size - 1)) {
+                    emptyLocation = (size * size) - 1;
+                    piece.gameObject.SetActive(false);
+                } else {
+                    // we want to map the UV coordinates appropriately, they are 0->1.
+                    float gap = gapThickness / 2;
+                    Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                    Vector2[] uv = new Vector2[4];
+                    // UV coord order: (0, 1), (1, 1), (0, 0), (1, 0)
+                    uv[0] = new Vector2((width * col) + gap, 1 - ((width * (row + 1)) - gap));
+                    uv[1] = new Vector2((width * (col + 1)) - gap, 1 - ((width * (row + 1)) - gap));
+                    uv[2] = new Vector2((width * col) + gap, 1 - ((width * row) + gap));
+                    uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
+                    // Assign our new UVs to the mesh.
+                    mesh.uv = uv;
+                }
             }
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
     // Start is called before the first frame update
     void Start()
     {
